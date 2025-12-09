e95fc5ad13f1238c1cb74c08173cb6509a02a8ee
jmarshallstewart <jmarshallstewart@gmail.com>
2025-12-09 14:19:16 -0600

    several bug fixes, some minor optimizations and style adjustments.
    1. Switched to Chebyshev distance as previous implementation used an inadmissible heuristic.
    
    2. Fixed a bug in drawTiles() that incorrectly used num rows instead of num columns for stride.
    
    3. Fixed typo in the initialization of numNodesSearched in pathfindingInit() and removed an usued variable there.
    
    4. Simplified function to get mouse coordinates.
    
    5. Switched "var" usage to const/let, and added "use strict";
    
    6. When searching neighbors, added an early out to avoid looking up the current node in the closed list again (not a bug, just a small optimization).
    
    7. cached DOM elements instead of relocating them each new state.
    
    8. Other minor commenting, cleanups, moved const values closer together.

--- pathfinding.js
+++ pathfinding.js
@@ -1,113 +1,127 @@
-var openList = null;
-var closedList = null;
-var numNodesSearched = 0;
-var path = null;
+"use strict";
 
-var statusSearching = 0;
-var statusPathFound = 1;
-var statusNoPathFound = 2;
+let openList = null;
+let closedList = null;
+let numNodesSearched = 0;
+let path = null;
+
+const statusSearching = 0;
+const statusPathFound = 1;
+const statusNoPathFound = 2;
 
 function pathfindingInit() {
-	openList = [];
-	closedList = [];
-	numNodesSearch = 0;
-	
-	path = null;
-	status = statusSearching;
+    // a list is fine for this tiny demo; consider a priority queue
+    // instead for larger grids or production use.
+    openList = []; 
+    closedList = [];
+    numNodesSearched = 0;
+    
+    path = null;
 }
 
 function PathfindingNode(row, col) {
-	this.row = row;
-	this.col = col;
-	this.parent = null;
-	this.f = 0;
-	this.g = 0;
-	this.h = 0;
+    this.row = row;
+    this.col = col;
+    this.parent = null;
+    this.f = 0;
+    this.g = 0;
+    this.h = 0;
 }
 
 // grid-based A* pathfinding.
 function stepPath(startRow, startCol, endRow, endCol, numRows, numCols, grid) {
 
-	if(openList.length > 0) {
-	
-		// find the node with the lowest cost and remove it from the open list.
-		openList.sort( function(a,b) { return a.f - b.f; } );
-		var currentNode = openList.shift();
-		
-		// if the current node is the destination, we're done. Return the node.
-		if(currentNode.row == endRow && currentNode.col == endCol) {
-			path = currentNode;
-			return statusPathFound;
-		}
-		
-		// add the lowest cost node to the closed list.
-		closedList.push(currentNode);
-		
-		// search 8 neighboring nodes
-		for(var r = -1; r <= 1; ++r) {
-			for(var c = -1; c <= 1; ++c) {
-				var nextRow = currentNode.row + r;
-				var nextCol = currentNode.col + c;
-				
-				// if the neighbor is within the bounds of the grid...
-				if (nextRow >= 0 && nextRow < numRows && nextCol >= 0 && nextCol < numCols) {
-					
-					// skip nodes that are not passable or are already on the closed list....
-					var isPassable = grid[nextCol + nextRow * numCols] == 0;
-					
-					if ( isPassable && !isInList(nextRow, nextCol, closedList) ) {
-						
-						var gCost = currentNode.g + 1;
-						var existingOpenNode = getFromList(nextRow, nextCol, openList);
-						
-						//if this neighbor node is not on open list, add it to the open list. Set its parent to currentNode.
-						if ( existingOpenNode == null ) {
-							var nextNode = new PathfindingNode(nextRow, nextCol);
-							nextNode.parent = currentNode;
-							nextNode.g = gCost;
-							nextNode.h = getEuclideanDistance(nextNode.row, nextNode.col, endRow, endCol);
-							nextNode.f = nextNode.g + nextNode.h;
-							openList.push(nextNode);
-						// otherwise, check to see if currentNode offers a shorter path to the node that
-						// is already in the open list. If so, set its parent to currentNode.
-						} else if (gCost < existingOpenNode.g) {
-							existingOpenNode.parent = currentNode;
-							existingOpenNode.g = gCost;
-							existingOpenNode.f = existingOpenNode.g + existingOpenNode.h;
-						}
-					}
-				}
-			}
-		}
-		
-		return statusSearching;
-	}
-	else
-	{
-		return statusNoPathFound;
-	}
+    if(openList.length > 0) {
+    
+        // find the node with the lowest cost and remove it from the open list.
+        openList.sort( function(a,b) { return a.f - b.f; } );
+        let currentNode = openList.shift();
+        
+        // if the current node is the destination, we're done. Return the node.
+        if(currentNode.row == endRow && currentNode.col == endCol) {
+            path = currentNode;
+            return statusPathFound;
+        }
+        
+        // add the lowest cost node to the closed list.
+        closedList.push(currentNode);
+        
+        // search 8 neighboring nodes
+        for(let r = -1; r <= 1; ++r) {
+            for(let c = -1; c <= 1; ++c) {
+                
+                // not strictly necessary for correctness, but skips one pass.
+                if (r == 0 && c == 0) {
+                    continue;
+                }
+                
+                let nextRow = currentNode.row + r;
+                let nextCol = currentNode.col + c;
+                
+                // if the neighbor is within the bounds of the grid...
+                if (nextRow >= 0 && nextRow < numRows && nextCol >= 0 && nextCol < numCols) {
+                    
+                    // skip nodes that are not passable or are already on the closed list....
+                    let isPassable = grid[nextCol + nextRow * numCols] == 0;
+                    
+                    if ( isPassable && !isInList(nextRow, nextCol, closedList) ) {
+                        
+                        let gCost = currentNode.g + 1;
+                        let existingOpenNode = getFromList(nextRow, nextCol, openList);
+                        
+                        //if this neighbor node is not on open list, add it to the open list. Set its parent to currentNode.
+                        if ( existingOpenNode == null ) {
+                            let nextNode = new PathfindingNode(nextRow, nextCol);
+                            nextNode.parent = currentNode;
+                            nextNode.g = gCost;
+                            nextNode.h = getChebyshevDistance(nextNode.row, nextNode.col, endRow, endCol);
+                            nextNode.f = nextNode.g + nextNode.h;
+                            openList.push(nextNode);
+                        // otherwise, check to see if currentNode offers a shorter path to the node that
+                        // is already in the open list. If so, set its parent to currentNode.
+                        } else if (gCost < existingOpenNode.g) {
+                            existingOpenNode.parent = currentNode;
+                            existingOpenNode.g = gCost;
+                            existingOpenNode.f = existingOpenNode.g + existingOpenNode.h;
+                        }
+                    }
+                }
+            }
+        }
+        
+        return statusSearching;
+    }
+    else
+    {
+        return statusNoPathFound;
+    }
 }
 
 function getFromList(row, col, list) {
-	for(var i = 0; i < list.length; ++i) {
-		if(list[i].row == row && list[i].col == col) {
-			return list[i];
-		}
-	}
-	
-	return null;
+    for(let i = 0; i < list.length; ++i) {
+        if(list[i].row == row && list[i].col == col) {
+            return list[i];
+        }
+    }
+    
+    return null;
 }
 
 function isInList(row, col, list) {
-	return getFromList(row, col, list) != null;
+    return getFromList(row, col, list) != null;
 }
 
-function getEuclideanDistance(startRow, startCol, endRow, endCol) {
-    var x = startRow - endRow;
-    var y = startCol - endCol;
-    return Math.sqrt(x * x + y * y);
-}
+//function getEuclideanDistance(startRow, startCol, endRow, endCol) {
+//    let x = startRow - endRow;
+//    let y = startCol - endCol;
+//    return Math.sqrt(x * x + y * y);
+//}
 
 //function getManhattanDistance(startRow, startCol, endRow, endCol) {
 //    return Math.abs(startRow - endRow) + Math.abs(startCol - endCol);
 //}
+
+// This is "number of moves a King needs to make on a chessboard to get from start to end"
+function getChebyshevDistance(startRow, startCol, endRow, endCol) {
+    return Math.max(Math.abs(startRow - endRow), Math.abs(startCol - endCol));
+}
